bb58f380654fa4c72708ec7a744699e3ec2c0409
Lautaro Dragan <lautaro.dragan@gmail.com>
2019-10-18 14:48:39 -0300

    tidy up tester a bit

--- tester/index.js
+++ tester/index.js
@@ -10,20 +10,19 @@ commander
   .option('-h, --host <path>', 'URL of the RPC', 'http://localhost:8545')
 
 async function main() {
-  console.log('Running Contract Tester')
   commander.parse(process.argv)
   const abiPath = resolve(commander.abi)
   const web3 = new Web3(commander.host)
   const blockNumber = await web3.eth.getBlockNumber()
-  console.log(`Contract ABI Path: ${abiPath}`)
-  console.log(`Contract Address: ${commander.address}`)
-  console.log(`Web3 Version ${web3.version}`)
-  console.log(`Block number ${blockNumber}`)
-
   const abi = readJSON(abiPath)
   const account = web3.eth.accounts.privateKeyToAccount('0x2cdfcc85aef24b085229deb1a07475878e384019273328a13592aedd1595c1c6')
   const contract = new web3.eth.Contract(abi, commander.address, { from: account.address })
 
+  console.log('Running Contract Tester')
+  console.log(`Contract ABI Path: ${abiPath}`)
+  console.log(`Contract Address: ${commander.address}`)
+  console.log(`Web3 Version ${web3.version}`)
+  console.log(`Block number ${blockNumber}`)
   console.log('Account Address', account.address)
 
   console.log('helloWorld():', await contract.methods.helloWorld().call())
